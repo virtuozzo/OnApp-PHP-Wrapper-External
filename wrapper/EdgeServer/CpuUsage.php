b29090da3c8480848c619cec43568859863982c8
Yuriy Yakubskiy <yuriy.yakubskiy@onapp.com>
2011-10-24 11:15:10 +0300

    Added OnApp_EdgeServer_CpuUsage class

---
+++ wrapper/EdgeServer/CpuUsage.php
@@ -0,0 +1,104 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Managing Cpu Usage
+ *
+ *
+ * @category	API WRAPPER
+ * @package		OnApp
+ * @subpackage	Cpu Usage
+ * @author		Yakubskiy Yuriy
+ * @copyright	(c) 2011 OnApp
+ * @link		http://www.onapp.com/
+ * @see			OnApp
+ */
+
+/**
+ * Cpu Usage
+ *
+ * The Cpu Usage class represents the Cpu Usage of the OnAPP installation.
+ *
+ * The ONAPP_CpuUsage class uses the following basic methods:
+ * {@link getList} and {@link load} and .
+ *
+ */
+class OnApp_EdgeServer_CpuUsage extends OnApp_VirtualMachine_CpuUsage {
+    public function __construct() {
+        parent::__construct();
+        $this->className = __CLASS__;
+    }
+
+    /**
+	 * Returns the URL Alias of the API Class that inherits the Class ONAPP
+	 *
+	 * @param string $action action name
+	 *
+	 * @return string API resource
+	 * @access public
+	 */
+	function getResource( $action = ONAPP_GETRESOURCE_DEFAULT ) {
+		switch( $action ) {
+			case ONAPP_GETRESOURCE_LIST:
+				/**
+				 * ROUTE :
+				 * @name cpu_usage_edge_servers
+				 * @method GET
+				 * @alias  /edge_servers/:id/cpu_usage(.:format)
+				 * @format  {:controller=>"edge_servers", :action=>"cpu_usage"}
+				 */
+				if( is_null( $this->_virtual_machine_id ) && is_null( $this->_obj->_virtual_machine_id ) ) {
+					$this->logger->error(
+						"getResource($action): argument _edge_server_id is not set.",
+						__FILE__,
+						__LINE__
+					);
+				}
+				else {
+					if( is_null( $this->_virtual_machine_id ) ) {
+						$this->_virtual_machine_id = $this->_obj->_virtual_machine_id;
+					}
+				}
+
+				$resource = 'edge_servers/' . $this->_virtual_machine_id . '/' . $this->_resource;
+				$this->logger->debug( 'getResource( ' . $action . ' ): return ' . $resource );
+				break;
+
+			default:
+				$resource = parent::getResource( $action );
+				break;
+		}
+
+		return $resource;
+	}
+
+    /**
+	 * Sends an API request to get the Objects. After requesting,
+	 * unserializes the received response into the array of Objects
+	 *
+	 * @param integer $virtual_machine_id Virtual Machine id
+	 *
+	 * @return mixed an array of Object instances on success. Otherwise false
+	 * @access public
+	 */
+	function getList( $virtual_machine_id = null ) {
+		if( is_null( $virtual_machine_id ) && !is_null( $this->_virtual_machine_id ) ) {
+			$virtual_machine_id = $this->_virtual_machine_id;
+		}
+
+		if( !is_null( $virtual_machine_id ) ) {
+			$this->_virtual_machine_id = $virtual_machine_id;
+
+			return parent::getList();
+		}
+		else {
+			$this->logger->error(
+				'getList: argument _virtual_machine_id not set.',
+				__FILE__,
+				__LINE__
+			);
+		}
+	}
+    
+}
