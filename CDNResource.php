4a539a2b11167d356458ccf49db55e3e80cecf83
Yuriy Yakubskiy <yuriy.yakubskiy@onapp.com>
2011-10-28 18:57:35 +0300

    CDNResource.php corrections

--- CDNResource.php
+++ CDNResource.php
@@ -80,21 +80,100 @@ class OnApp_CDNResource extends OnApp {
 					'cdn_hostname' => array(
 						ONAPP_FIELD_MAP => '_cdn_hostname',
 						ONAPP_FIELD_TYPE => 'string',
+                        //ONAPP_FIELD_REQUIRED => true,
 					),
                     'aflexi_resource_id' => array(
-						ONAPP_FIELD_MAP => 'aflexi_resource_id',
-						ONAPP_FIELD_TYPE => 'integer',
-					),
-                    'aflexi_resource_id' => array(
-						ONAPP_FIELD_MAP => 'aflexi_resource_id',
-						ONAPP_FIELD_TYPE => 'integer',
-					),
-                    'aflexi_resource_id' => array(
-						ONAPP_FIELD_MAP => 'aflexi_resource_id',
+						ONAPP_FIELD_MAP => '_aflexi_resource_id',
 						ONAPP_FIELD_TYPE => 'integer',
 					),
 
+                    // fields to create CDN Resource
+                    'hotlink_policy' => array(
+						ONAPP_FIELD_MAP => '_hotlink_policy',
+						ONAPP_FIELD_TYPE => 'string',
+					),
+                    'ip_access_policy' => array(
+						ONAPP_FIELD_MAP => '_ip_access_policy',
+						ONAPP_FIELD_TYPE => 'string',
+					),
+                    'ip_addresses' => array(
+						ONAPP_FIELD_MAP => '_ip_addresses',
+						ONAPP_FIELD_TYPE => 'string',
+					),
+                    'edge_group_ids' => array(
+						ONAPP_FIELD_MAP => '_edge_group_ids',
+						ONAPP_FIELD_TYPE => 'array',
+                        //ONAPP_FIELD_REQUIRED => true,
 
+					),
+                    'resource_type' => array(
+						ONAPP_FIELD_MAP => '_resource_type',
+						ONAPP_FIELD_TYPE => 'integer',
+                        //ONAPP_FIELD_REQUIRED => true,
+						ONAPP_FIELD_DEFAULT_VALUE => 'HTTP_PULL'
+					),
+                    'country_access_policy' => array(
+						ONAPP_FIELD_MAP => '_country_access_policy',
+						ONAPP_FIELD_TYPE => 'string',
+					),
+                    'contries' => array(
+						ONAPP_FIELD_MAP => '_contries',
+						ONAPP_FIELD_TYPE => 'array',
+					),
+                    'origin' => array(
+						ONAPP_FIELD_MAP => '_origin',
+						ONAPP_FIELD_TYPE => 'string',
+                        //ONAPP_FIELD_REQUIRED => true,
+					),
+                    'advanced_settings' => array(
+						ONAPP_FIELD_MAP => '_advanced_settings',
+						ONAPP_FIELD_TYPE => 'boolean',
+					),
+                    'hotlink_policy' => array(
+						ONAPP_FIELD_MAP => '_hotlink_policy',
+						ONAPP_FIELD_TYPE => 'string',
+					),
+                    'domains' => array(
+						ONAPP_FIELD_MAP => '_domains',
+						ONAPP_FIELD_TYPE => 'string',
+					),
+                    'url_signing_on' => array(
+						ONAPP_FIELD_MAP => '_url_signing_on',
+						ONAPP_FIELD_TYPE => 'boolean',
+					),
+                    'url_signing_key' => array(
+						ONAPP_FIELD_MAP => '_url_signing_key',
+						ONAPP_FIELD_TYPE => 'string',
+					),
+                    'cache_expiry' => array(
+						ONAPP_FIELD_MAP => '_cache_expiry',
+						ONAPP_FIELD_TYPE => 'integer',
+					),
+                    'password_on' => array(
+						ONAPP_FIELD_MAP => '_password_on',
+						ONAPP_FIELD_TYPE => 'boolean',
+					),
+                    'password_unauthorized_html' => array(
+						ONAPP_FIELD_MAP => '_password_unauthorized_html',
+						ONAPP_FIELD_TYPE => 'string',
+					),
+                    'pass' => array(
+						ONAPP_FIELD_MAP => '_pass',
+						ONAPP_FIELD_TYPE => 'array',
+					),
+                    'user' => array(
+						ONAPP_FIELD_MAP => '_user',
+						ONAPP_FIELD_TYPE => 'array',
+					),
+                    'form_pass' => array(
+						ONAPP_FIELD_MAP => '_form_pass',
+						ONAPP_FIELD_TYPE => 'array',
+					),
+                    'countries' => array(
+						ONAPP_FIELD_MAP => '_countries',
+						ONAPP_FIELD_TYPE => 'array',
+					),
+                    
 				);
                 break;
 		}
