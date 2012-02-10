440a3ad4634d2228989dcb25f101ea686882cd5f
Yuriy Yakubskiy <yuriy.yakubskiy@onapp.com>
2012-02-10 16:39:40 +0200

    Added OnApp_CDNResource_AvailableEdgeGroup adn OnApp_CDNResource_AvailableEdgeGroup_Location classes

---
+++ wrapper/CDNResource/AvailableEdgeGroup.php
@@ -0,0 +1,108 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Represents Edge Groups Assigned to the current logined user's Billling Plan
+ *
+ *
+ * @category	API WRAPPER
+ * @package		OnApp
+ * @author		Yakubskiy Yuriy
+ * @copyright	(c) 2011 OnApp
+ * @link		http://www.onapp.com/
+ * @see			OnApp
+ */
+
+/**
+ * Managing Edge Groups
+ *
+ * The Available Edge Group class represents the Edge groups.
+ * The OnApp_CDNResource_AvailableEdgeGroup class is the parent of the OnApp class.
+ *
+ * The OnApp_CDNResource_AvailableEdgeGroup uses the following basic methods:
+ * {@link getList}.
+ *
+ */
+
+
+class OnApp_CDNResource_AvailableEdgeGroup extends OnApp {
+	/**
+	 * root tag used in the API request
+	 *
+	 * @var string
+	 */
+	var $_tagRoot = 'edge_group';
+
+	/**
+	 * alias processing the object data
+	 *
+	 * @var string
+	 */
+	var $_resource = 'cdn_resources/available_edge_groups';
+
+	public function __construct() {
+		parent::__construct();
+		$this->className = __CLASS__;
+	}
+
+	/**
+	 * API Fields description
+	 *
+	 * @param string|float $version OnApp API version
+	 * @param string $className current class' name
+	 * @return array
+	 */
+	public function initFields( $version = null, $className = '' ) {
+		switch( $version ) {
+			case '2.3':
+				$this->fields = array(
+                    'label' => array(
+						ONAPP_FIELD_MAP => '_label',
+						ONAPP_FIELD_REQUIRED => true,
+						ONAPP_FIELD_DEFAULT_VALUE => ''
+					),
+                    'created_at' => array(
+						ONAPP_FIELD_MAP => '_created_at',
+						ONAPP_FIELD_TYPE => 'datetime',
+						ONAPP_FIELD_READ_ONLY => true
+					),
+                    'updated_at' => array(
+						ONAPP_FIELD_MAP => '_updated_at',
+						ONAPP_FIELD_TYPE => 'datetime',
+						ONAPP_FIELD_READ_ONLY => true
+					),
+					'id' => array(
+						ONAPP_FIELD_MAP => '_id',
+						ONAPP_FIELD_TYPE => 'integer',
+						ONAPP_FIELD_READ_ONLY => true,
+					),
+					'edge_group_locations' => array(
+						ONAPP_FIELD_MAP => '_edge_group_locations',
+						ONAPP_FIELD_TYPE => 'array',
+                        ONAPP_FIELD_CLASS => 'CDNResource_AvailableEdgeGroup_Location'
+					),
+				);
+                break;
+		}
+
+		parent::initFields( $version, __CLASS__ );
+		return $this->fields;
+	}
+   
+	/**
+	 * Activates action performed with object
+	 *
+	 * @param string $action_name the name of action
+	 *
+	 * @access public
+	 */
+	function activate( $action_name ) {
+		switch( $action_name ) {
+			case ONAPP_ACTIVATE_LOAD:
+			case ONAPP_ACTIVATE_SAVE:
+			case ONAPP_ACTIVATE_DELETE:
+				exit( 'Call to undefined method ' . __CLASS__ . '::' . $action_name . '()' );
+				break;
+		}
+	}
+}
