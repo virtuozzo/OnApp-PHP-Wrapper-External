99f5a12a7e3d2a5e11883ed1058673b3db0333c0
Yuriy Yakubskiy <yuriy.yakubskiy@onapp.com>
2012-03-02 13:01:24 +0200

    Added CDNResource_Bandwidth class

---
+++ CDNResource/Bandwidth.php
@@ -0,0 +1,113 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * CDN Resource Bandwidth Statistics
+ *
+ * @category	API WRAPPER
+ * @package		OnApp
+ * @subpackage	CDNResource
+ * @author		Yakubskiy Yuriy
+ * @copyright	(c) 2012 OnApp
+ * @link		http://www.onapp.com/
+ * @see			OnApp
+ */
+
+/**
+ * CDN Resource Bandwidth Statistics
+ *
+ * The OnApp_CDNResource_BillingStatistic uses the following basic methods:
+ * {@link getList}.
+ */
+
+
+class OnApp_CDNResource_Bandwidth extends OnApp {
+	/**
+	 * root tag used in the API request
+	 *
+	 * @var string
+	 */
+	var $_tagRoot = 'stat';
+
+	/**
+	 * alias processing the object data
+	 *
+	 * @var string
+	 */
+	var $_resource = 'cdn_resources/bandwidth';
+
+	public function __construct() {
+		parent::__construct();
+		$this->className = __CLASS__;
+	}
+
+	/**
+	 * API Fields description
+	 *
+	 * @param string|float $version OnApp API version
+	 * @param string $className current class' name
+	 * @return array
+	 */
+	public function initFields( $version = null, $className = '' ) {
+		switch( $version ) {
+			case '2.0':
+			case '2.1':
+            case '2.2':
+			    break;
+			case '2.3':
+				$this->fields = array(
+                    'non_cached' => array(
+						ONAPP_FIELD_MAP => '_non_cached',
+						ONAPP_FIELD_TYPE => 'string',
+						ONAPP_FIELD_READ_ONLY => true,
+					),
+					'date' => array(
+						ONAPP_FIELD_MAP => '_date',
+						ONAPP_FIELD_TYPE => 'string',
+						ONAPP_FIELD_READ_ONLY => true,
+					),
+					'cached' => array(
+						ONAPP_FIELD_MAP => '_cached',
+						ONAPP_FIELD_TYPE => 'string',
+						ONAPP_FIELD_READ_ONLY => true,
+					),
+				);
+				$this->fields = $this->initFields( 2.1 );
+				break;
+		}
+
+		parent::initFields( $version, __CLASS__ );
+		return $this->fields;
+	}
+
+    /**
+	 * Sends an API request to get the Objects. After requesting,
+	 * unserializes the received response into the array of Objects
+	 *
+	 * @param array $url_args [start, end, type, resource_type, resources[] ]
+	 *
+	 * @return mixed an array of Object instances on success. Otherwise false
+	 * @access public
+	 */
+	public function getList( $url_args = null ) {
+	    return parent::getList( null, $url_args );
+	}
+
+	/**
+	 * Activates action performed with object
+	 *
+	 * @param string $action_name the name of action
+	 *
+	 * @access public
+	 */
+	function activate( $action_name ) {
+		switch( $action_name ) {
+			case ONAPP_ACTIVATE_LOAD:
+			case ONAPP_ACTIVATE_SAVE:
+			case ONAPP_ACTIVATE_DELETE:
+				exit( 'Call to undefined method ' . __CLASS__ . '::' . $action_name . '()' );
+				break;
+		}
+	}
+
+}
