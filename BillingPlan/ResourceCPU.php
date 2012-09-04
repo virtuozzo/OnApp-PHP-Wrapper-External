8bed3a29aca46991feb5f82e4d64ba751398ea1d
Lev Bartashevsky <l@l>
2012-09-04 19:23:50 +0300

    add files

---
+++ BillingPlan/ResourceCPU.php
@@ -0,0 +1,27 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Billing Plan Base Resources
+ *
+ * @category    API wrapper
+ * @package     OnApp
+ * @subpackage  BillingPlan
+ * @author      Andrew Yatskovets
+ * @copyright   (c) 2011 OnApp
+ * @link        http://www.onapp.com/
+ * @see         OnApp
+ */
+
+/**
+ * The OnApp_BillingPlan_ResourceCpu class uses the following basic methods:
+ * {@link load}, {@link save}, {@link delete}, and {@link getList}.
+ *
+ * For full fields reference and curl request details visit: ( http://help.onapp.com/manual.php?m=2 )
+ */
+class OnApp_BillingPlan_ResourceCPU extends OnApp_BillingPlan_BaseResource {
+	/**
+	 * @property string resource_class
+	 */
+	protected $resource_class = 'Resource::CPU';
+}
---
+++ BillingPlan/ResourceCPUShare.php
@@ -0,0 +1,27 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Billing Plan Base Resources
+ *
+ * @category    API wrapper
+ * @package     OnApp
+ * @subpackage  BillingPlan
+ * @author      Andrew Yatskovets
+ * @copyright   (c) 2011 OnApp
+ * @link        http://www.onapp.com/
+ * @see         OnApp
+ */
+
+/**
+ * The OnApp_BillingPlan_ResourceCpuShare class uses the following basic methods:
+ * {@link load}, {@link save}, {@link delete}, and {@link getList}.
+ *
+ * For full fields reference and curl request details visit: ( http://help.onapp.com/manual.php?m=2 )
+ */
+class OnApp_BillingPlan_ResourceCPUShare extends OnApp_BillingPlan_BaseResource {
+	/**
+	 * @property string resource_class
+	 */
+	protected $resource_class = 'Resource::CPUShare';
+}
