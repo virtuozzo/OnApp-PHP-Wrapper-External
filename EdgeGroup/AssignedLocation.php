22eeae6470312a9ea3601c0f09dd12769b759534
Lev Bartashevsky <devman@iMac.local>
2013-05-31 13:30:05 +0300

    fix typo in filename

---
+++ EdgeGroup/AssignedLocation.php
@@ -0,0 +1,45 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Manages Assigned Location
+ *
+ *
+ * @category    API wrapper
+ * @package     OnApp
+ * @subpackage  EdgeGroup
+ * @author      Yakubskiy Yuriy
+ * @copyright   (c) 2011 OnApp
+ * @link        http://www.onapp.com/
+ * @see         OnApp
+ */
+
+/**
+ * Edge Server
+ *
+ * The OnApp_EdgeGroup_AssignedLocation class represents the OnApp_EdgeGroup_AssignedLocation of the OnAPP installation.
+ *
+ * The OnApp_EdgeGroup_AssignedLocation class uses no basic methods and is nested of OnApp_EdgeGroup class
+ *
+ *
+ */
+class OnApp_EdgeGroup_AssignedLocation extends OnApp_EdgeGroup_Location {
+	/**
+	 * root tag used in the API request
+	 *
+	 * @var string
+	 */
+	var $_tagRoot = '';
+
+	/**
+	 * alias processing the object data
+	 *
+	 * @var string
+	 */
+	var $_resource = '';
+
+	public function __construct() {
+		parent::__construct();
+		$this->className = __CLASS__;
+	}
+}
