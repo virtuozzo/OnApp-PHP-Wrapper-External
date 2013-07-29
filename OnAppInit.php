a79c6f7fe76cf1702b0fa051157454b368b48d52
Lev Bartashevsky <devman@iMac.local>
2013-07-29 18:43:12 +0300

    reformat code

--- OnAppInit.php
+++ OnAppInit.php
@@ -9,49 +9,49 @@
  * @link        http://www.onapp.com/
  */
 if( ! defined( 'ONAPP_WRAPPER_ROOT_DIR' ) ) {
-    /**
-     * Specify the wrapper root dir
-     */
-    define( 'ONAPP_WRAPPER_ROOT_DIR', dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
+	/**
+	 * Specify the wrapper root dir
+	 */
+	define( 'ONAPP_WRAPPER_ROOT_DIR', dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
 
-    /**
-     * Automatically load requested class
-     *
-     * @param string $className requested class' name
-     *
-     * @return bool
-     */
-    function OnAppAutoLoad( $className ) {
-        // compatibility with legacy code
-        $className = str_replace( 'ONAPP', 'OnApp', $className );
+	/**
+	 * Automatically load requested class
+	 *
+	 * @param string $className requested class' name
+	 *
+	 * @return bool
+	 */
+	function OnAppAutoLoad( $className ) {
+		// compatibility with legacy code
+		$className = str_replace( 'ONAPP', 'OnApp', $className );
 
-        $path = str_replace( 'OnApp_', '', $className );
-        $path = explode( '_', $path );
-        $path = ONAPP_WRAPPER_ROOT_DIR . implode( DIRECTORY_SEPARATOR, $path ) . '.php';
+		$path = str_replace( 'OnApp_', '', $className );
+		$path = explode( '_', $path );
+		$path = ONAPP_WRAPPER_ROOT_DIR . implode( DIRECTORY_SEPARATOR, $path ) . '.php';
 
-        if(file_exists( $path ) ) {
-            require $path;
-    
-            if( class_exists( $className ) ) {
-                return true;
-            }
-            //todo add loging instead of printing
-        }
-        return false;
-    }
+		if( file_exists( $path ) ) {
+			require $path;
 
-    /**
-     * Register autoload handler
-     */
-    spl_autoload_register( 'OnAppAutoLoad' );
+			if( class_exists( $className ) ) {
+				return true;
+			}
+			//todo add loging instead of printing
+		}
+		return false;
+	}
 
-    /**
-     * Detect if the code run in CLI for testing purposes
-     */
-    if( defined( 'STDIN' ) ) {
-        define( 'IS_CLI', true );
-    }
-    else {
-        define( 'IS_CLI', false );
-    }
+	/**
+	 * Register autoload handler
+	 */
+	spl_autoload_register( 'OnAppAutoLoad' );
+
+	/**
+	 * Detect if the code run in CLI for testing purposes
+	 */
+	if( defined( 'STDIN' ) ) {
+		define( 'IS_CLI', true );
+	}
+	else {
+		define( 'IS_CLI', false );
+	}
 }
