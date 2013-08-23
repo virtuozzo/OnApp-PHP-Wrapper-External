14de74d5bc352699f71f71e978df5ef5eec0aebe
Andrew Yatskovets <jtsandrew@gmail.com>
2013-08-23 17:23:24 +0300

    Add class OnApp_CDNResource_Instructions

---
+++ CDNResource/Instructions.php
@@ -0,0 +1,146 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * @category    API wrapper
+ * @package     OnApp
+ * @author      Andrew Yatskovets
+ * @subpackage  CDNResource
+ * @copyright   (c) 2013 OnApp
+ * @link        http://www.onapp.com/
+ * @see         OnApp
+ */
+class OnApp_CDNResource_Instructions extends OnApp {
+    /**
+     * root tag used in the API request
+     *
+     * @var string
+     */
+    var $_tagRoot = 'cdn_resource';
+
+    /**
+     * alias processing the object data
+     *
+     * @var string
+     */
+    var $_resource = 'instructions';
+
+    public function __construct() {
+        parent::__construct();
+        $this->className = __CLASS__;
+    }
+
+    /**
+     * API Fields description
+     *
+     * @param string|float $version   OnApp API version
+     * @param string       $className current class' name
+     *
+     * @return array
+     */
+    public function initFields( $version = null, $className = '' ) {
+        switch( $version ) {
+            case 3.1:
+                $this->fields = array(
+                    'cdn_hostname' => array(
+                        ONAPP_FIELD_MAP => '_cdn_hostname',
+                        ONAPP_FIELD_REQUIRED => true,
+                        ONAPP_FIELD_DEFAULT_VALUE => ''
+                    ),
+                    'cname' => array(
+                        ONAPP_FIELD_MAP => '_cdn_cname',
+                        ONAPP_FIELD_REQUIRED => true,
+                        ONAPP_FIELD_DEFAULT_VALUE => ''
+                    ),
+                    'created_at' => array(
+                        ONAPP_FIELD_MAP => '_created_at',
+                        ONAPP_FIELD_TYPE => 'datetime',
+                        ONAPP_FIELD_READ_ONLY => true
+                    ),
+                    'resource_type' => array(
+                        ONAPP_FIELD_MAP => '_resource_type',
+                        ONAPP_FIELD_REQUIRED => true,
+                        ONAPP_FIELD_DEFAULT_VALUE => ''
+                    ),
+                    'user_id' => array(
+                        ONAPP_FIELD_MAP => '_user_id',
+                        ONAPP_FIELD_REQUIRED => true,
+                        ONAPP_FIELD_DEFAULT_VALUE => ''
+                    ),
+                    'last_24h_cost' => array(
+                        ONAPP_FIELD_MAP => '_last_24h_cost',
+                        ONAPP_FIELD_REQUIRED => true,
+                        ONAPP_FIELD_DEFAULT_VALUE => ''
+                    ),
+                    'cdn_reference' => array(
+                        ONAPP_FIELD_MAP => '_cdn_reference',
+                        ONAPP_FIELD_REQUIRED => true,
+                        ONAPP_FIELD_DEFAULT_VALUE => ''
+                    ),
+                    'publishing_point' => array(
+                        ONAPP_FIELD_MAP => '_publishing_point',
+                        ONAPP_FIELD_REQUIRED => true,
+                        ONAPP_FIELD_DEFAULT_VALUE => ''
+                    ),
+                    'updated_at' => array(
+                        ONAPP_FIELD_MAP => '_updated_at',
+                        ONAPP_FIELD_TYPE => 'datetime',
+                        ONAPP_FIELD_READ_ONLY => true
+                    ),
+                    'id' => array(
+                        ONAPP_FIELD_MAP => '_id',
+                        ONAPP_FIELD_TYPE => 'integer',
+                        ONAPP_FIELD_READ_ONLY => true,
+                    ),
+                    'instructions' => array(
+                        ONAPP_FIELD_MAP => '_instructions',
+                        ONAPP_FIELD_TYPE => 'array',
+                        ONAPP_FIELD_CLASS => 'OnApp_CDNResource_Instructions_Instructions'
+                    ),
+                );
+                break;
+        }
+
+        parent::initFields( $version, __CLASS__ );
+        return $this->fields;
+    }
+
+    /**
+     * Returns the URL Alias of the API Class that inherits the OnApp class
+     *
+     * @param string $action action name
+     *
+     * @return string API resource
+     * @access public
+     */
+    function getResource( $action = ONAPP_GETRESOURCE_DEFAULT ) {
+        switch( $action ) {
+            case ONAPP_GETRESOURCE_LOAD:
+                $resource = 'cdn_resources/' . $this->_id . '/' . $this->_resource;
+                $this->logger->debug( 'getResource( ' . $action . ' ): return ' . $resource );
+                break;
+            default:
+                $resource = parent::getResource( $action );
+                break;
+        }
+        return $resource;
+    }
+
+
+    /**
+     * Activates action performed with object
+     *
+     * @param string $action_name the name of action
+     *
+     * @access public
+     */
+    function activate( $action_name ) {
+        switch( $action_name ) {
+            case ONAPP_ACTIVATE_GETLIST:
+            case ONAPP_ACTIVATE_SAVE:
+            case ONAPP_ACTIVATE_DELETE:
+                exit( 'Call to undefined method ' . __CLASS__ . '::' . $action_name . '()' );
+                break;
+        }
+    }
+}
